b43b867f644334c3395bf101f622a9259a9c748a
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-30 10:54:39 +0530

    create bst

---
+++ BST.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hashtable
+{
+    internal class BST
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Table And Binary Search Tree");
+            Binary<int> bst = new Binary<int>(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            Console.WriteLine("Binary Search Tree ");
+            bst.Display();
+            Console.ReadLine();
+        }
+    }
+}
